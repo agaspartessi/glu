73c7fe33a07417d70a6b661f103bc160656b53e7
Gaspar Tessi <agaspartessi@gmail.com>
2026-06-09 12:44:26 -0300

    volviendo atras cambio

--- javascript/glu_onetopic_custom_index.js
+++ javascript/glu_onetopic_custom_index.js
@@ -271,11 +271,11 @@ document.addEventListener('DOMContentLoaded', function () {
 
         var nav = document.createElement('nav');
         nav.className = 'glu-custom-courseindex';
-        nav.setAttribute('aria-label', 'Course chapters');
+        nav.setAttribute('aria-label', 'Course index');
 
         var title = document.createElement('div');
         title.className = 'glu-custom-courseindex__title';
-        title.textContent = 'Course chapters';
+        title.textContent = 'Course index';
 
         var list = document.createElement('ul');
         list.className = 'glu-custom-courseindex__list';
